3c1aa1105a6cd0f70648d251f0af08d6603b8cbb
kuku20 <lluu3790@gmail.com>
2023-02-18 21:36:17 -0600

    using edit ngrx

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { switchMap } from 'rxjs';
+import { Appstate, Books } from '../book/store/book';
+import { setAPIStatus, updateBookApi } from '../book/store/book.actions';
+import { selectAppState, selectBookById } from '../book/store/book.selector';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store,
+    private appStore: Store<Appstate>
+  ) {}
 
-  constructor() { }
+  bookForm: Books = {
+    id: 0,
+    author: '',
+    name: '',
+    cost: 0,
+  };
 
   ngOnInit(): void {
+    let fetchData$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        var id = Number(params.get('id'));
+        return this.store.pipe(select(selectBookById(id)));
+      })
+    );
+    fetchData$.subscribe((data) => {
+      if (data) {
+        this.bookForm = { ...data };
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
+  udapte() {
+    this.store.dispatch(
+      updateBookApi({ updateBook: { ...this.bookForm } })
+    );
+    let apiStatus$ = this.appStore.pipe(select(selectAppState));
+    apiStatus$.subscribe((apState) => {
+      if (apState.apiStatus == 'success') {
+        this.appStore.dispatch(
+          setAPIStatus({ apiStatus: { apiResponseMessage: '', apiStatus: '' } })
+        );
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
