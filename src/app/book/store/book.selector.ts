f0e2d96ff12b294b94cf9ed83230f3dbc8f98e0b
kuku20 <lluu3790@gmail.com>
2023-02-18 21:24:42 -0600

    edit selector

--- src/app/book/store/book.selector.ts
+++ src/app/book/store/book.selector.ts
@@ -1,6 +1,15 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { Appstate, Books } from './book';
- 
+
 export const selectBooks = createFeatureSelector<Books[]>('mybooks');
 
 export const selectAppState = createFeatureSelector<Appstate>('appState');
+
+export const selectBookById = (bookId: number) =>
+  createSelector(selectBooks, (books: Books[]) => {
+    var bookbyId = books.filter((_) => _.id == bookId);
+    if (bookbyId.length == 0) {
+      return null;
+    }
+    return bookbyId[0];
+  });
