db14c3fc71231a1672e521e68327e2456c093c04
kuku20 <lluu3790@gmail.com>
2023-02-18 19:47:15 -0600

    standalone lazy load

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
